49a6b3dc380e6df1bfcf254b38fbbf67333f1d97
keycabs <248224463+keycabs@users.noreply.github.com>
2026-09-03 01:49:51 +0000

    Print statement ended to early

--- src/Text02.java
+++ src/Text02.java
@@ -6,7 +6,6 @@ public class Text02 {
             System.out.println("2");
         }
         System.out.println("3");
-        }
-    System.out.println ("4");
+        System.out.println ("4");
     }
 }
