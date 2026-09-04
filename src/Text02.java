78d94584cd0cbc66423b99a5d7e199e7add7ffb2
keycabs <248224463+keycabs@users.noreply.github.com>
2026-09-04 03:35:10 +0000

    Reinstate comment

--- src/Text02.java
+++ src/Text02.java
@@ -1,11 +1,14 @@
+// Prints out numbers 1-4 on separate lines
+
 public class Text02 {
 
     public static void main(String[] args) {
         System.out.println("1");
         {
-            System.out.println("2");
+            System.out.println("2")
         }
         System.out.println("3");
-        System.out.println ("4");
+        }
+    System.out.println ("4");
     }
 }
